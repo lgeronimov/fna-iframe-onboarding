146f771e8007fb9d8cc24e14bc7d9dd361a1c002
Luis Gerónimo <lgeronimo@na-at.com.mx>
2022-08-23 20:21:45 -0500

    feat: FNA full integration

--- index.js
+++ index.js
@@ -5,23 +5,29 @@ window.onload = function () {
 // events available
 const EVENT_MODULE = {
   PROCESS_INIT: "PROCESS_INIT",
-  PROCESS_ERROR: "PROCESS_ERROR",
   PROCESS_COMPLETED: "PROCESS_COMPLETED",
-  ENTRY_PATH: "ENTRY_PATH"
 }
 
 // subscribe to message event to recive the events from the iframe
 window.addEventListener("message", (message) => {
   // IMPORTANT: check the origin of the data!
   if (message.origin.includes("firmaautografa.com")) {
-
+    if (message.data.event === EVENT_MODULE.PROCESS_INIT) {
+      // only informative
+      console.log("Process init");
+    } else if (message.data.event === EVENT_MODULE.PROCESS_COMPLETED) {
+      // end of the process
+      console.log("Process completed");
+      console.log(message.data.data);
+      alert(JSON.stringify(message.data.data))
+    } 
   } else return;
 });
 
 function initIframe() {
   // get iframe 
   const iframe = document.getElementById("fad-fna-iframe");
-  // url of fad, uat - uatwebfad4.firmaautografa.com, prod - mobile.firmaautografa.com
+  // url 
   const url = 'https://devfna.firmaautografa.com/'
   // set src to iframe
   iframe.src = url;
